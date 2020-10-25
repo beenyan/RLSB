3a13a48e633448a0a93a4b146ad4e1f32d1af6c6
BeenYan <asdfghjkl42045@gmail.com>
2020-10-25 11:41:53 +0800

    PPT

--- 教學.js
+++ 教學.js
@@ -1,6 +1,19 @@
-0 <= Math.random() < 1 // 0 ~ 1 之間的亂數
-Math.floor(4.5) == 4 // 無條件捨去
-Math.ceil(0.1) == 1 // 無條件進位
-Math.sqrt(9) == 3 // 取得根號
-Math.sqrt(3,3) == 3**3 // 取得次方
-Math.Pi == 3.141592653589793 // PI
+'use strict';
+/** @type {HTMLCanvasElement} */ // 宣告作業環境
+class Round {
+    constructor(drg){
+        let def = {
+            age: 10,
+        }
+        Object.assign(def,drg);
+        Object.assign(this,def);
+    }
+    draw(){
+
+    }
+}
+let round = new Round({name:"kiki"});
+
+const canvas = document.getElementById('canvas'); // 取得畫布
+const ctx = canvas.getContext('2d'); // 宣告2D畫布
+round.draw();
